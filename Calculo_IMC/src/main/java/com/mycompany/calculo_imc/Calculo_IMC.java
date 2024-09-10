bf3418eb0209549e5bbbca21c578d3d6abb65884
Sanmmmm <samngof@gmail.com>
2024-09-09 23:38:24 -0300

    Add files via upload

---
+++ Calculo_IMC/src/main/java/com/mycompany/calculo_imc/Calculo_IMC.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculo_imc;
+
+import com.mycompany.calculo_imc.Tela.Tela;
+
+/**
+ *
+ * @author sanmg
+ */
+public class Calculo_IMC {
+
+    public static void main(String[] args) {
+      java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Tela().setVisible(true);
+            }
+        });
+     
+    }
+}
